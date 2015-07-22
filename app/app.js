77edcc02ccab6f424705d1bb65e1d9c6ba51ddb4
dglushenkov <dglushenkov@speroteck.com>
2015-07-22 18:27:29 +0300

    add angular-ui

--- app/app.js
+++ app/app.js
@@ -6,81 +6,12 @@ $(function() {
         e.preventDefault();
     });
 
-    $(window).on('scroll.stickyNav', function(e) {
-        if ($(window).scrollTop() > stickyNav.getNavBottom()) {
-            stickyNav.makeSticky();
-        } else {
-            stickyNav.removeSticky();
-        }
-    });
-
-    $('.dropdown').on('show.bs.dropdown', function(e) {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
-    });
-
-    $('.dropdown').on('hide.bs.dropdown', function(e) {
-        var menu = $(this).find('.dropdown-menu').first().stop(true, true);
-        menu.addClass('slidingUp')
-            .slideUp(150, function() {
-                $(this).removeClass('slidingUp');
-            })
-    });
-
-
-    var stickyNav = {
-        isSticky: false,
-        nav: $('.main-nav-container'),
-        placeholder: $('.main-nav-placeholder'),
-
-        getNavBottom: function() {
-            var container = (this.isSticky) ? this.placeholder : this.nav;
-            var bottom = container.height() + container.offset().top;
-            return bottom;
-        },
-
-        makeSticky: function() {
-            if (this.isSticky) return;
-
-            this.placeholder.height(this.nav.height());
-            this.placeholder.css({
-                marginTop: this.nav.css('margin-top'),
-                marginBottom: this.nav.css('margin-bottom')
-            })
-            this.placeholder.show();
-            this.nav.addClass('fix-nav');
-            this.closeDropdowns();
-
-            this.isSticky = true;
-        },
-
-        closeDropdowns: function() {
-            this.nav.find('.dropdown-toggle').each(function() {
-                var $this = $(this);
-                if ($this.closest('.dropdown').hasClass('open')) {
-                    console.log('isVisible');
-                    $this.dropdown('toggle');
-                }
-            });
-        },
-
-        removeSticky: function() {
-            if (!this.isSticky) return;
-
-            this.placeholder.hide();
-            this.nav.removeClass('fix-nav');
-            this.closeDropdowns();2
-
-            this.isSticky = false;
-        }
-    }
-
-    $(window).trigger('scroll.stickyNav');
 });
 
 //=======================================================================
 // Angular
 //=======================================================================
-var app = angular.module('luckystore', ['ngRoute']);
+var app = angular.module('luckystore', ['ngRoute', 'ui.bootstrap', 'ngAnimate']);
 
 app.config(['$routeProvider',
     function($routeProvider) {
@@ -166,6 +97,83 @@ function owlCarouselInit(selector) {
     });
 }
 
+app.controller('NavbarCtrl', ['$scope', function($scope) {
+    $scope.isCollapsed = true;
+
+    $scope.toggleNav = function() {
+        $scope.isCollapsed = !$scope.isCollapsed;
+    }
+
+    $(window).on('scroll.stickyNav', function(e) {
+        if ($(window).scrollTop() > stickyNav.getNavBottom()) {
+            stickyNav.makeSticky();
+        } else {
+            stickyNav.removeSticky();
+        }
+    });
+
+    $('.dropdown').on('show.bs.dropdown', function(e) {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
+    });
+
+    $('.dropdown').on('hide.bs.dropdown', function(e) {
+        var menu = $(this).find('.dropdown-menu').first().stop(true, true);
+        menu.addClass('slidingUp')
+            .slideUp(150, function() {
+                $(this).removeClass('slidingUp');
+            })
+    });
+
+
+    var stickyNav = {
+        isSticky: false,
+        nav: $('.main-nav-container'),
+        placeholder: $('.main-nav-placeholder'),
+
+        getNavBottom: function() {
+            var container = (this.isSticky) ? this.placeholder : this.nav;
+            var bottom = container.height() + container.offset().top;
+            return bottom;
+        },
+
+        makeSticky: function() {
+            if (this.isSticky) return;
+
+            this.placeholder.height(this.nav.height());
+            this.placeholder.css({
+                marginTop: this.nav.css('margin-top'),
+                marginBottom: this.nav.css('margin-bottom')
+            })
+            this.placeholder.show();
+            this.nav.addClass('fix-nav');
+            this.closeDropdowns();
+
+            this.isSticky = true;
+        },
+
+        closeDropdowns: function() {
+            this.nav.find('.dropdown-toggle').each(function() {
+                var $this = $(this);
+                if ($this.closest('.dropdown').hasClass('open')) {
+                    console.log('isVisible');
+                    $this.dropdown('toggle');
+                }
+            });
+        },
+
+        removeSticky: function() {
+            if (!this.isSticky) return;
+
+            this.placeholder.hide();
+            this.nav.removeClass('fix-nav');
+            this.closeDropdowns();2
+
+            this.isSticky = false;
+        }
+    }
+
+    $(window).trigger('scroll.stickyNav');
+}]);
 app.controller('SalesCtrl', ['$scope', '$http', '$routeParams',
     function($scope, $http, $routeParams) {
         $http.get('products/products.json').success(function(data) {
